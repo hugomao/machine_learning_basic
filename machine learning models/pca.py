b664985d19ff9665e00f0536f4c78763c2435bae
HMAO <hugomao1220@gmail.com>
2018-02-02 09:41:09 -0800

    Update pca.py

--- machine learning models/pca.py
+++ machine learning models/pca.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import pdist, squareform
 
 def pca(x):
 	num_data,num_var = x.shape
@@ -24,7 +25,28 @@ def pca(x):
 
 #https://zhuanlan.zhihu.com/p/21583787
 #explanation of kernel pca: http://blog.csdn.net/lanchunhui/article/details/50492482
-def kernel_pca(x):
+#apply rbf kernel here
+def kernel_pca(x,gamma,k):
+    #calculate pairwise distance
+    sq_dists = pdist(x, metric='sqeuclidean')
+    mat_sq_dists = squareform(sq_dists)
+    #calculate kernel value
+    K = np.exp(-gamma*mat_sq_dists)
+    
+    N = X.shape[0]
+    one_N = np.ones((N, N))/N
+    #centralize kernel
+    K = K-one_N.dot(K)-K.dot(one_N)+one_N.dot(K).dot(one_N)
+    
+    #calculate e/ev for kernel
+    e,ev = np.linalg.eigh(K)
+    alphas = np.stack((ev[:,-i] for i in range(1,k+1)),axis = 1)
+    lambdas = [e[-i] for i in range(1, k+1)]
+    return alphas, lambdas
+
+def kernel_pca_proj(x_new,x,alphas,lambdas,gamma):
+    K_new = np.exp(-gamma*np.sum((x-x_new)**2, 1))
+    return K_new.dot(alphas / lambdas)
 
 
 
